8203b0b0ffda1f6ce84d68d421c5f3b527fc4cf4
John Hornick <jhornick@live.com>
2023-01-27 07:08:51 -0500

    IDs and delete functionality added.

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const fs = require('fs');
 const express = require('express');
-const app = express();
+const uniqid = require('uniqid');
 const path = require('path');
+
+const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
 
 app.use(express.json());
 //app.use(express.urlencoded({ extended: true }));
+function SaveData(data) {
+    fs.writeFile('./db/db.json',JSON.stringify(data),err=>{
+        if(err) {
+            console.log(err);
+        }
+    });
+}
 
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
@@ -25,13 +34,22 @@ app.get('/api/notes',(req,res) => {
 app.post('/api/notes',(req,res) => {
     const incomingData = req.body;
     const data = require('./db/db.json');
+    incomingData.id = uniqid();
     data.push(incomingData);
-    fs.writeFile('./db/db.json',JSON.stringify(data),err=>{
-        if(err) {
-            console.log(err);
-        }
-    });
+    SaveData(data);
     res.json('POST request handled');
+});
+
+app.delete('/api/notes/:id',(req,res) => {
+    const data = require('./db/db.json');
+    for(let i = 0; i < data.length; i++) {
+        if(data[i].id == req.params.id) {
+            data.splice(i,1);
+            break;
+        }
+    }
+    SaveData(data);
+    res.json('received');
 });
 
 app.listen(PORT,err=>{
